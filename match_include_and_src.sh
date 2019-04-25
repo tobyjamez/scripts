cc20866f0a90cc81ab8899496476e9dc2ccafc17
tobyjamez <tobyswjames@gmail.com>
2019-04-25 13:53:52 +0100

    Add include directives by default

--- match_include_and_src.sh
+++ match_include_and_src.sh
@@ -3,7 +3,7 @@
 # Ensures that the include and src directories contain the correct files for each other
 for filename in ./include/*; do
     if [ ! -f "./src/${filename:10:-2}cc" ]; then
-        touch "./src/${filename:10:-2}cc"
+        echo "#include \"${filename:10}\"" >> "./src/${filename:10:-2}cc"
     fi
     done;
 
