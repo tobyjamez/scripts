e049d313bdbea55ff200b8467543c38bfd35a935
Toby James <toby.james@ukaea.uk>
2021-03-23 18:56:47 +0000

    Remove useless script

--- GetSongInfo.sh
+++
@@ -1,6 +0,0 @@
-Artist=$(qdbus org.mpris.clementine /Player org.freedesktop.MediaPlayer.GetMetadata | grep -i artist)
-echo ${Artist[@]^}
-Album=$(qdbus org.mpris.clementine /Player org.freedesktop.MediaPlayer.GetMetadata | grep -i album)
-echo ${Album[@]^}
-Album=$(qdbus org.mpris.clementine /Player org.freedesktop.MediaPlayer.GetMetadata | grep -i title)
-echo ${Album[@]^}
